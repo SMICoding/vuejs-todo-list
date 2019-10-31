b740b100a710d5fa071d6c3aaa8b163cdbe65ce8
schkiho <schkiho@yahoo.de>
2019-10-31 12:39:37 +0100

    initial commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <form @submit="addTodo">
+      <input type="text" v-model="title" name="title" placeholder="Add Todo...">
+      <input type="submit" value="Submit" class="btn">
+    </form>
+  </div>
+</template>
+
+<script>
+//import uuid from "uuid" for without jsonplaceholder
+
+export default {
+  name:"AddTodo",
+  data(){
+    return{
+      title:''
+    }
+  },
+  methods:{
+    addTodo(e){
+      e.preventDefault();
+      const newTodo = {
+        // id: uuid.v4(), for without jsonplaceholder
+        title:this.title,
+        completed:false
+      }
+      // send up to parent
+      this.$emit('add-todo',newTodo)
+      // clear input field
+      this.title = "";
+    }
+  }
+}
+</script>
+
+<style scoped>
+  form{
+    display:felx;
+  }
+
+  input[type="text"]{
+    flex:10;
+    padding:5px;
+  }
+
+  input[type="submit"]{
+    flex:2;
+  }
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the TodoList app v1.0.0. It's part of the Vue crash course on Youtube</p>
+  </div>
+</template>
+
+<style scoped>
+  .about{
+    text-align: center;
+  }
+
+  .about h1{
+    margin:20px 0px;
+  }
+</style>
